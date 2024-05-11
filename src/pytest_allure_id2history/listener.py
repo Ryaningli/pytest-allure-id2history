1f2412190845d7450d93d904b162db7f8c2d3773
lining <lining@etsnet.cn>
2024-05-11 18:40:59 +0800

    BUGFIX: fixture teardown: 'Function' object has no attribute 'callspec'

--- src/pytest_allure_id2history/listener.py
+++ src/pytest_allure_id2history/listener.py
@@ -13,7 +13,9 @@ class AllureOverwriteListener:
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_teardown(self, item):
         yield
-        # noinspection PyProtectedMember
-        uuid = self._allure_listener._cache.get(item.nodeid)
-        test_result = self._allure_listener.allure_logger.get_test(uuid)
-        test_result.historyId = md5(test_result.fullName, item.callspec.id)
+        if hasattr(item, 'callspec'):
+            case_id = item.callspec.id
+            # noinspection PyProtectedMember
+            uuid = self._allure_listener._cache.get(item.nodeid)
+            test_result = self._allure_listener.allure_logger.get_test(uuid)
+            test_result.historyId = md5(test_result.fullName, case_id)
